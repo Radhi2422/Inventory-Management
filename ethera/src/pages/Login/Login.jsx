2cf4dff9e0c4b05f5d5cfd6c481a8501609fd01d
Radhika Mittal <106824237+Radhi2422@users.noreply.github.com>
2026-07-29 13:48:16 +0530

    Refactor Login component and update imports

--- ethera/src/pages/Login/Login.jsx
+++ ethera/src/pages/Login/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
 import { loginUser } from "../../services/authService";
-import { analytics } from "../analytics/segment";
+import { analytics } from "../../analytics/segment";
 
 const Login = () => {
   const [userId, setUserId] = useState("");
